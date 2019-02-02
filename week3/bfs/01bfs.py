6fe18857f7a4378e8ef46b6ec1c16bd2553f52dc
kyle <kyle1james@gmail.com>
2019-02-02 09:52:40 -0500

    bfs

--- week3/bfs/01bfs.py
+++ week3/bfs/01bfs.py
@@ -1,19 +1,23 @@
-adjacency_list = {1:[2], 4:[1], 2:[3], 3:[1]}
+#https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/lecture-videos/MIT6_006F11_lec13.pdf
+adjacency_list = {1:[2], 4:[1], 2:[3], 3:[4]}
 
 def distance(adj, s, t):
-    #write your code here
-    dist = [len(adj)] * len(adj)
-    dist[s] = 0
-    queue = []
-    queue.append(s)
-    while queue:
-        u = queue.pop(0)
-        for v in adj[u]:
-            if dist[v] == len(adj):
-                queue.append(v)
-                dist[v] = dist[u] + 1
-    if dist[t] != len(adj):
-        return dist[t]
-    return -1
+    level = {s: 0}
+    parent = {s: None}
+    i = 1
+    frontier = [s]
+    while frontier:
+        next = []
+        for u in frontier:
+            for v in adj[u]:
+                if v not in level:
+                    level[v] = i
+                    parent[v] = u
+                    next.append(v)
+                    if v == t:
+                        return level[v]
+        frontier = next
+        i += 1
 
-distance(adjacency_list, 2,4)
+
+print(distance(adjacency_list, 1,4))
