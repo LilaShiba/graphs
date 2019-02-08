9b75912892ba6bac99d26be1c05265e5f0e6a3cb
Kyle <kyle1james@gmail.com>
2019-02-08 09:50:05 -0500

    dij

---
+++ sandbox/dij.py
@@ -0,0 +1,54 @@
+#https://brilliant.org/wiki/dijkstras-short-path-finder/
+# https://pastebin.com/3Q9rqGHA
+# https://medium.com/basecs/finding-the-shortest-path-with-a-little-help-from-dijkstra-613149fbdc8e
+graph = {
+            's': {'a': 2, 'b': 1},
+            'a': {'s': 3, 'b': 4, 'c':8},
+            'b': {'s': 4, 'a': 2, 'd': 2},
+            'c': {'a': 2, 'd': 7, 't': 4},
+            'd': {'b': 1, 'c': 11, 't': 5},
+            't': {'c': 3, 'd': 5}
+            }
+
+graph2 = {
+        1:{2:7, 3:5},
+        2:{3:2},
+        3:{}
+}
+
+
+def dijkstra(adj, source):
+    #dist=[float('inf')]*len(adj)
+    shortest_distance = {}
+    predecessor = {}
+    unseenNodes = adj
+    path = []
+
+    # set all unexplored nodes to inf
+    for vertex in adj:
+        shortest_distance[vertex] = float('inf')
+    shortest_distance[source] = 0
+
+    # search time
+    while unseenNodes:
+        minNode = None
+        # grab first unseenNode
+        for node in unseenNodes:
+            # set minNode
+            if minNode is None:
+                minNode = node
+            elif shortest_distance[node] < shortest_distance[minNode]:
+                minNode = node
+        # check neighbors
+        for childNode, weight in adj[minNode].items():
+            # relax
+            if weight + shortest_distance[minNode] < shortest_distance[childNode]:
+                shortest_distance[childNode] = weight + shortest_distance[minNode]
+                # increase minNode for each level
+                predecessor[childNode] = minNode
+                #print(minNode)
+        unseenNodes.pop(minNode)
+    return shortest_distance
+
+
+print(dijkstra(graph,'a'))
