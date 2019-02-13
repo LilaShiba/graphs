de9507f441a279463c794753843a2f084cbaa955
Kyle <kyle1james@gmail.com>
2019-02-13 08:45:21 -0500

    build trie notes

--- string_algorithms/sandbox/wk1/trie.py
+++ string_algorithms/sandbox/wk1/trie.py
@@ -1,10 +1,12 @@
 def make_trie(pattern):
+    # {}
     tree = dict()
+    # {0: {}}
     tree[0] = {}
     index = 1
 
     for pattern in patterns:
-		# current = {}
+		# current = { 0:{current} }
         current = tree[0]
         for letter in pattern:
             if letter in current.keys():
@@ -23,7 +25,47 @@ def make_trie(pattern):
                 index = index + 1
     return tree
 
+
+
+def solve(text, n, patterns):
+    result = []
+    trie = build_trie(patterns)
+
+    n = len(text)
+    for i in range(n):
+        if prefix_trie_matching(text[i:], trie):
+            result.append(i)
+
+    return result
+
+
+def prefix_trie_matching(text, trie):
+    idx = 0
+    symbol = text[idx]
+    current = trie[0]
+
+    while True:
+        if not current:
+            return True
+        elif symbol in current.keys():
+            current = trie[current[symbol]]
+            idx = idx + 1
+            if idx < len(text):
+                symbol = text[idx]
+            else:
+                symbol = '@'
+        else:
+            return False
+
+
+
+
 trie = build_trie(['ATAGA', 'ATC', 'GAT'])
+
+
+
+
+
 
 def prefix_trie_matching(text, trie):
     idx = 0
