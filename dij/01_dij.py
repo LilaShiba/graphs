4eccff6f83575538de4a86890b42c30648a56376
Kyle  James <kjames@DWIGHT.edu>
2019-03-12 10:43:54 -0400

    dij

--- dij/01_dij.py
+++ dij/01_dij.py
@@ -1,37 +1,29 @@
-import random
-import time
-
-start_time = time.time()
-
 def dijsktra(graph):
     length = len(graph)
-    start = (0,0)
     goal = (length-1, length-1)
-
     visited = {}
-    path = {}
-
     nodes = []
+    graph = [[int(x) for x in y]for y in graph]
+
+    # nodes = [[(x,y) for y in range(length)] for x in range(length)]
+    # nodes = [item for sublist in nodes for item in sublist]
 
     for x in range(length):
         for y in range(length):
             nodes.append((x,y))
-            #visited[(x,y)] = float('Inf')
     visited[(0,0)] = 0
 
 
     while nodes:
         # find minNode
-        minNode = None
+        minNode = nodes[0]
         for node in nodes:
             if node in visited:
-                if minNode == None:
-                    minNode = node
-                elif visited[node] < visited[minNode]:
+                if visited[node] < visited[minNode]:
                     minNode = node
 
-        if minNode == (length-1, length-1):
-            return visited[length-1, length-1], path
+        if minNode == goal:
+            return visited[goal]
 
         nodes.remove(minNode)
         current_weight = visited[minNode]
@@ -40,15 +32,23 @@ def dijsktra(graph):
         directions = [(x, y+1), (x, y-1), (x+1, y), (x-1, y)]
         for cx,cy in directions:
             if (cx,cy) in nodes:
-                weight = visited[(x,y)] + abs(matrix[cx][cy]- matrix[x][y])
+                weight = visited[(x,y)] + abs(graph[cx][cy]- graph[x][y])
                 if (cx,cy) not in visited or weight < visited[(cx,cy)]:
                     visited[(cx, cy)] = weight
-                    path[minNode] = (cx,cy)
-    return visited[length-1, length-1], path
+                    #path[minNode] = (cx,cy)
 
-matrix = [[random.randint(0,10) for x in range(5)]for y in range(5)]
-num, path = dijsktra(matrix)
 
+
+
+
+
+import random
+import time
+
+start_time = time.time()
+
+matrix = [[random.randint(0,10) for x in range(50)]for y in range(50)]
+num = dijsktra(matrix)
+
+print(num)
 print("---%s seconds" %(time.time()- start_time))
-
-print(num, path)
