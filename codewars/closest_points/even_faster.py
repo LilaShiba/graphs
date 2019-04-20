0a4bd49917f26b8b60b9c2cf4c37f46beceffb1b
kyle <kyle1james@gmail.com>
2019-04-20 18:28:23 -0400

    :)

--- codewars/closest_points/even_faster.py
+++ codewars/closest_points/even_faster.py
@@ -3,9 +3,9 @@
 # https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-046j-design-and-analysis-of-algorithms-spring-2012/lecture-notes/MIT6_046JS12_lec23.pdf
 # http://web.cs.ucdavis.edu/~bai/ECS122A/Closestpair.pdf
 # https://www.youtube.com/watch?v=xi-WF07rAQw
-import big_num
+import bigboy
 
-big_guy = big_num.points# todo o nlogn time
+big_guy = bigboy.points# todo o nlogn time
 # Done: merge sort
 import math
 def brute_force(points):
@@ -62,6 +62,7 @@ def merge_sort(arr, pos):
 def cp_helper(X,Y):
   length = len(X)
   # base case
+
   if length <= 3:
     return brute_force(X)
 
@@ -71,12 +72,22 @@ def cp_helper(X,Y):
   xl = X[:mid]
   xr = X[mid:]
   # sort y cords of the sorted x cords
-  yl = [x for x in Y if x in xl]
-  yr = [x for x in Y if x in xr]
+  # yl = [x for x in Y if x in xl]
+  # yr = [x for x in Y if x in xr]
+
+  yl,yr = [],[]
+
+  midpoint = X[mid][0]
+
+  for x in Y:
+    if x[0] <= midpoint:
+          yl.append(x)
+    else:
+        yr.append(x)
 
   # divide recursively
   (pl, ql) = cp_helper(xl,yl)
-  (pr,qr) = cp_helper(xr,yr)
+  (pr, qr) = cp_helper(xr,yr)
 
 
   # distance
@@ -90,13 +101,13 @@ def cp_helper(X,Y):
   ys = [x for x in Y if x[0] - d <= X[mid][0]]
 
   # checking points that cross left/right line for min
-  e = float('Inf')
+  e = 1000000
 
   for i in range(len(ys)-1):
     p = ys[i]
     new_min = min(i+7, len(ys))
     new_loop = ys[i+1:new_min]
-    for q in new_loop:
+    for q in ys[i+1:new_min]:
       loop_dist = math.sqrt( ( p[0]- q[0] )**2 + ( p[1] - q[1] )**2 )
       if e > loop_dist:
         e = loop_dist
@@ -113,6 +124,9 @@ def cp_helper(X,Y):
 
 
 def closest_pair(points):
+    if len(points) <= 2:
+        return points
+
     if len(points) <= 3:
         # no need for divide_conquer if n < 3
         return brute_force(points)
@@ -125,19 +139,19 @@ def closest_pair(points):
         # Do everything yourself because otherwise what do you learn?
         # Run merge sort as it is O(nlogn)
         # pass in arr and pos of where to sort, e.g. x or y
-        sorted_x = merge_sort(arr_x,0)
-        sorted_y = merge_sort(arr_y,1)
+        # sorted_x = merge_sort(arr_x,0)
+        # sorted_y = merge_sort(arr_y,1)
 
         # the pythonic way
-        #sorted_x.sort(key=lambda x: x[0])
-        #sorted_y.sort(key=lambda x: x[1])
+        arr_x.sort(key=lambda x: x[0])
+        arr_y.sort(key=lambda x: x[1])
 
         # Basic visual test passes
         # print(sorted_x)
         # print(sorted_y)
 
         # pass the dirty work to
-        return cp_helper(sorted_x,sorted_y)
+        return cp_helper(arr_x,arr_y)
 
 
 
@@ -166,9 +180,5 @@ points2 =(
                 (7, 9)  # G
         )
 #expected =((5, 5), (5, 5))
-
-points3 =((523.5139692759321, -296.7442311708965), (-133.05821275435656, -425.43580429241126), (317.2449514411967, -360.8834782870809), (409.2591050707605, -110.19926849874086), (-55.77620609750431, 11.666782902636495), (207.05573000407796, -166.29872475550832), (86.95274797772912, 15.417411093001775), (-0.5426634713212408, -421.11205060411714), (-107.81856221322658, -124.05310216790309), (290.93360338461116, -510.01037111229937), (175.58073583784568, -297.6878118765567), (174.5281500180918, 164.3521715375582), (-98.04792032251606, -232.56592836443747), (49.08553172409406, -142.01881337746335), (466.51898654331836, 55.4641076875605), (630.8648958305262, 45.266420798565974), (219.72144506224996, -57.249191371286855), (590.1807567592095, -92.06517641397801), (182.52130748170026, -477.34102479503906), (478.7286538716342, -442.028554044102), (-44.2541695361207, 39.679626039299194), (444.1327604995383, -524.928399501247), (39.738421016527425, 196.71103703844148), (28.993426535260376, -308.33020858219453), (396.98561227882897, -203.05641477727266), (262.79917111151826, 98.50239375327888))
-
-
 
 print(closest_pair(big_guy))
