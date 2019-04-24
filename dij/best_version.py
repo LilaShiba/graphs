3836a961455a9727d8d4121b02f60f43ca47014c
Kyle <kyle1james@gmail.com>
2019-04-24 12:01:19 -0400

    wow

--- dij/best_version.py
+++ dij/best_version.py
@@ -4,7 +4,7 @@ start_time = time.time()
 def path_finder(matrix):
 
     length = len(matrix)
-    start = (0,0,0)
+    start = (0,0,0,0)
     goal = (length-1, length-1)
 
     parent = {}
@@ -14,10 +14,10 @@ def path_finder(matrix):
 
     while unseenNodes:
         #minNode = heapq.heappop(unseenNodes)
-        unseenNodes = sorted(unseenNodes, key = lambda x: x[2])
+        unseenNodes = sorted(unseenNodes, key = lambda x: x[3])
         minNode = unseenNodes.pop(0)
 
-        x,y,current_weight = minNode
+        x,y,current_weight,_ = minNode
         px, py = x,y
 
         neighbors = ((x-1,y),(x,y-1),(x+1,y),(x,y+1))
@@ -25,17 +25,18 @@ def path_finder(matrix):
 
         for cx,cy in real_neighbors:
             cost = current_weight + matrix[cx][cy]
+            dist = abs(goal[0] - cx) + abs(goal[1]-cy)
             if (cx,cy) not in parent or cost < visited[(cx,cy)]:
                 visited[(cx,cy)] = cost
-                unseenNodes.append((cx,cy,cost))
+                unseenNodes.append((cx,cy,cost,dist))
                 parent[(cx,cy)] = (px,py)
 
                 if (x,y) == goal:
-                    return visited[goal] + matrix[x][y]
+                    return ('cost is', visited[goal] + matrix[x][y])
 
 
 import random, pprint
-matrix = [[0,1,2,3,4,5], [0,1,2,3,4,5], [0,1,2,3,4,5],[0,1,2,3,4,5],[0,1,1,1,1,1]]
-pprint.pprint(matrix)
+matrix = [[x + 1 for x in range(100)] for y in range(100)]
 
 print(path_finder(matrix))
+print("--- %s seconds ---" % (time.time() - start_time))
